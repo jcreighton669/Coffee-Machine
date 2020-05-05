072ef07e9b4a77893e901b94937c364016db89ff
Justin Allen Creighton <46484674+jcreighton669@users.noreply.github.com>
2020-05-05 15:35:44 -0400

    May 5, 2020
    After lunch break

---
+++ Problems/Calculating an expression/task.py
@@ -0,0 +1,5 @@
+# put your python code here
+n = int(input())
+
+n = ((n + 1) * n + 2) * n + 3
+print(n)
---
+++ Problems/Desks/task.py
@@ -0,0 +1,8 @@
+# put your python code here
+room_one_students = int(input())
+room_two_students = int(input())
+room_three_students = int(input())
+
+room_one_desks = 
+
+
