341f0f7767385eb9594742be9c97db8fa6d37524
Justin Allen Creighton <46484674+jcreighton669@users.noreply.github.com>
2020-05-06 15:47:56 -0400

    May 6, 2020
     After lunch

--- Coffee Machine/task/machine/coffee_machine.py
+++ Coffee Machine/task/machine/coffee_machine.py
@@ -1,15 +1,35 @@
 # Write your code here
-water_for_coffee = 200
-milk_for_coffee = 50
-beans_for_coffee = 15
+print("Write how many ml of water the coffee machine has:")
+water_in_machine = int(input())
 
-cups_wanted = int(input("Write how many cups of coffee you will need: "))
+print("Write how many ml of milk the coffee machine has:")
+milk_in_machine = int(input())
 
-water_needed = water_for_coffee * cups_wanted
-milk_needed = milk_for_coffee * cups_wanted
-beans_needed = beans_for_coffee * cups_wanted
+print("Write how many grams of coffee beans the coffee machine has:")
+beans_in_machine = int(input())
 
-print("For {} cups of coffee you will need:".format(cups_wanted))
-print("{} ml of water".format(water_needed))
-print("{} ml of milk".format(milk_needed))
-print("{} g of coffee beans".format(beans_needed))
+print("Write how many cups of coffee you will need:")
+cups_wanted = int(input())
+
+WATER_PER_CUP = 200
+MILK_PER_CUP = 50
+BEANS_PER_CUP = 15
+
+water_needed = WATER_PER_CUP * cups_wanted
+milk_needed = MILK_PER_CUP * cups_wanted
+beans_needed = BEANS_PER_CUP * cups_wanted
+
+water_cups_available = water_in_machine // water_needed
+milk_cups_available = milk_in_machine // milk_needed
+bean_cups_available = beans_in_machine // beans_needed
+
+possible_cups = min(water_cups_available, milk_cups_available, bean_cups_available)
+
+if possible_cups == cups_wanted:
+    print("Yes, I can make that amount of coffee")
+elif possible_cups < cups_wanted:
+    print("No, I can make only {} cups of coffee".format(possible_cups))
+elif possible_cups > cups_wanted:
+    print("Yes, I can make that amount of coffee (and even {} more than that)"
+          .format((possible_cups - cups_wanted)))
+
