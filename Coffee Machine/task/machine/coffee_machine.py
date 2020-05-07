e190bf554f0b7d109f3eee753c91347e14243a8b
Justin Allen Creighton <46484674+jcreighton669@users.noreply.github.com>
2020-05-07 10:07:22 -0400

    May 7, 2020
    Before exam

--- Coffee Machine/task/machine/coffee_machine.py
+++ Coffee Machine/task/machine/coffee_machine.py
@@ -1,22 +1,74 @@
-print('Write how many ml of water the coffee machine has:')
-water = int(input())
-print('Write how many ml of milk the coffee machine has:')
-milk = int(input())
-print('Write how many grams of coffee beans the coffee machine has:')
-beans = int(input())
-print('Write how many cups of coffee you will need:')
-cups = int(input())
+CASH_ON_HAND = 550
+WATER_ON_HAND = 400
+MILK_ON_HAND = 540
+BEANS_ON_HAND = 120
+CUPS_ON_HAND = 9
 
-enough_water = water // 200
-enough_milk = milk // 50
-enough_beans = beans // 15
+print("The coffee machine has:")
+print("{} of water".format(WATER_ON_HAND))
+print("{} of milk".format(MILK_ON_HAND))
+print("{} of coffee beans".format(BEANS_ON_HAND))
+print("{} of disposable cups".format(CUPS_ON_HAND))
+print("{} of money".format(CASH_ON_HAND))
 
-enough_for = [enough_water, enough_milk, enough_beans]
+print("\nWrite action (buy, fill, take): ")
+choice = input()
+if choice.lower() == "buy":
+    print("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino: ")
+    drink_option = int(input())
+    if drink_option == 1:
+        WATER_ON_HAND -= 250
+        BEANS_ON_HAND -= 16
+        CUPS_ON_HAND -= 1
+        CASH_ON_HAND += 4
+    elif drink_option == 2:
+        WATER_ON_HAND -= 350
+        MILK_ON_HAND -= 75
+        BEANS_ON_HAND -= 20
+        CUPS_ON_HAND -= 1
+        CASH_ON_HAND += 7
+    elif drink_option == 3:
+        WATER_ON_HAND -= 200
+        MILK_ON_HAND -= 100
+        BEANS_ON_HAND -= 12
+        CUPS_ON_HAND -= 1
+        CASH_ON_HAND += 6
 
-if min(enough_for) == cups:
-    print('Yes, I can make that amount of coffee')
-elif min(enough_for) < cups:
-    print('No, I can make only' + str(min(enough_for)) + 'cups of coffee')
-else:
-    print('Yes, I can make that amount of coffee (and even ' + str(min(
-        enough_for) - 1) + ' more than that)')
+    print("The coffee machine has:")
+    print("{} of water".format(WATER_ON_HAND))
+    print("{} of milk".format(MILK_ON_HAND))
+    print("{} of coffee beans".format(BEANS_ON_HAND))
+    print("{} of disposable cups".format(CUPS_ON_HAND))
+    print("{} of money".format(CASH_ON_HAND))
+
+elif choice.lower() == "fill":
+    print("Write how many ml of water do you want to add: ")
+    fill_water = int(input())
+    print("Write how many ml of milk do you want to add: ")
+    fill_milk = int(input())
+    print("Write how many grams of coffee beans do you want to add: ")
+    fill_beans = int(input())
+    print("Write how many disposable cups of coffee do you want to add: ")
+    fill_cups = int(input())
+
+    WATER_ON_HAND += fill_water
+    MILK_ON_HAND += fill_milk
+    BEANS_ON_HAND += fill_beans
+    CUPS_ON_HAND += fill_cups
+    print("\nThe coffee machine has:")
+    print("{} of water".format(WATER_ON_HAND))
+    print("{} of milk".format(MILK_ON_HAND))
+    print("{} of coffee beans".format(BEANS_ON_HAND))
+    print("{} of disposable cups".format(CUPS_ON_HAND))
+    print("{} of money".format(CASH_ON_HAND))
+
+elif choice.lower() == "take":
+    print("I gave you ${}".format(CASH_ON_HAND))
+    CASH_ON_HAND = 0
+
+    print("\nThe coffee machine has:")
+    print("{} of water".format(WATER_ON_HAND))
+    print("{} of milk".format(MILK_ON_HAND))
+    print("{} of coffee beans".format(BEANS_ON_HAND))
+    print("{} of disposable cups".format(CUPS_ON_HAND))
+    print("{} of money".format(CASH_ON_HAND))
